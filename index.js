56ab9c5266ec13cad0515e0afb3769cd2f5958d8
Walter Aab <aabwalter@yahoo.com>
2021-05-15 16:03:45 -0500

    commit tree cbea6f18ddf983afa1f21b1f9ce371651cfa33dd

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+// const record = [
+//     {year: "2018", result: "L"},
+//     {year: "2017", result: "W"},
+//     {year: "2016", result: "N/A"}
+//     //...
+//   ] 
+
+superbowlWin = (arrayOfObjects) => {
+    let result = arrayOfObjects.find(record => record.result === "W")
+    return !!result ? result.year : undefined
+}
